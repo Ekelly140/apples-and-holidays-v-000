978191eb6ff06ae90bbaa5cbad7dbf6eaed174e6
ERIC KELLY <eakelly220@gmail.com>
2018-04-19 18:25:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,16 +52,16 @@ def all_supplies_in_holidays(holiday_hash)
     value.each do |holiday, supplies|
       #puts "  #{holiday.capitalize}: #{supply}" 
 
-      puts "  #{holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")}: #{supplies.join(", ")}"
+      puts "  #{holiday.to_s.split("_").each {|w| w.capitalize}.join(" ")}: #{supplies.join(", ")}"
       end 
   end 
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+ bbq_holidays
+ holiday.hash.each do |seasons, values|
+   if values.each 
 end
 
 
