68aa8879cafb0b972cb959207348b33b206f71ff
ERIC KELLY <eakelly220@gmail.com>
2018-04-19 18:36:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_holidays_with_bbq(holiday_hash)
  bbq_holidays = []
  holiday_hash.each do |seasons, values|
    values.map do |holiday, supplies| 
-    puts holiday
+    binding.pry
      bbq_holidays << holiday
    end 
   end 
